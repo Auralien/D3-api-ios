ebdc162f0152dc7c0ecade6feef1938d2b2afe20
Maxim Mikheev <maxmikheev@gmail.com>
2013-02-12 22:21:39 +0400

    Hero class improved
    1. Add hero's properties.
    2. Add hero's init methods.

--- D3-api-ios/D3Hero.h
+++ D3-api-ios/D3Hero.h
@@ -8,6 +8,53 @@
 
 #import "D3Object.h"
 
+typedef enum {
+    kD3HeroGenderUndefined,
+    kD3HeroGenderMale,
+    kD3HeroGenderFemale
+} D3HeroGender;
+
+typedef enum {
+    kD3HeroClassUndefined,
+    kD3HeroClassBarbarian,
+    kD3HeroClassWitchDoctor,
+    kD3HeroClassWizard,
+    kD3HeroClassMonk,
+    kD3HeroClassDemonHunter
+} D3HeroClass;
+
 @interface D3Hero : D3Object
+
+/// Hero unique ID
+@property (nonatomic) NSInteger heroID;
+/// Hero name
+@property (nonatomic, copy) NSString *heroName;
+/// Hero class from enumeration @a D3HeroClass
+@property (nonatomic) D3HeroClass heroClass;
+/// Hero gender from enumeration @a D3HeroGender
+@property (nonatomic) D3HeroGender heroGender;
+/// Hero level
+@property (nonatomic) NSInteger heroLevel;
+/// Hero hardcore flag, YES for hardcore
+@property (nonatomic) BOOL isHardcoreHero;
+/// Date and time for hero's information last update
+@property (nonatomic, strong) NSDate *heroLastUpdated;
+/// Elite mobs killed number
+@property (nonatomic) NSInteger elitesKilled;
+
+#pragma mark - Init Methods
+
+/// Initialize hero object with zero values
+- (id)init;
+
+/// Initialize hero object with defined values
+- (id)initWithHeroID:(NSInteger)heroIDVal
+            heroName:(NSString *)heroNameVal
+           heroClass:(D3HeroClass)heroClassVal
+          heroGender:(D3HeroGender)heroGenderVal
+           heroLevel:(NSInteger)heroLevelVal
+        hardcoreFlag:(BOOL)isHardcoreHeroVal
+     heroLastUpdated:(NSDate *)heroLastUpdatedVal
+        elitesKilled:(NSInteger)elitesKilledVal;
 
 @end
--- D3-api-ios/D3Hero.m
+++ D3-api-ios/D3Hero.m
@@ -10,4 +10,42 @@
 
 @implementation D3Hero
 
+@synthesize heroID, heroName, heroClass, heroGender, heroLevel, isHardcoreHero, heroLastUpdated, elitesKilled;
+
+#pragma mark - Init Methods
+
+/// Initialize hero object with zero values
+- (id)init {
+    return [self initWithHeroID:0
+                       heroName:@""
+                      heroClass:kD3HeroClassUndefined
+                     heroGender:kD3HeroGenderUndefined
+                      heroLevel:0
+                   hardcoreFlag:NO
+                heroLastUpdated:[NSDate dateWithTimeIntervalSince1970:0]
+                   elitesKilled:0];
+}
+
+/// Initialize hero object with defined values
+- (id)initWithHeroID:(NSInteger)heroIDVal
+            heroName:(NSString *)heroNameVal
+           heroClass:(D3HeroClass)heroClassVal
+          heroGender:(D3HeroGender)heroGenderVal
+           heroLevel:(NSInteger)heroLevelVal
+        hardcoreFlag:(BOOL)isHardcoreHeroVal
+     heroLastUpdated:(NSDate *)heroLastUpdatedVal
+        elitesKilled:(NSInteger)elitesKilledVal {
+    if (self = [super init]) {
+        [self setHeroID:heroIDVal];
+        [self setHeroName:heroNameVal];
+        [self setHeroClass:heroClassVal];
+        [self setHeroGender:heroGenderVal];
+        [self setHeroLevel:heroLevelVal];
+        [self setIsHardcoreHero:isHardcoreHeroVal];
+        [self setHeroLastUpdated:heroLastUpdatedVal];
+        [self setElitesKilled:elitesKilledVal];
+    }
+    return self;
+}
+
 @end

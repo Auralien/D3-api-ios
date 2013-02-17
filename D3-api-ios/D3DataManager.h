eea8bf553f9f1bd39b4d5236faa7ddd3ed0c1bca
Maxim Mikheev <maxmikheev@gmail.com>
2013-02-17 09:04:54 +0400

    Add D3DataManager class

---
+++ D3-api-ios/D3DataManager.h
@@ -0,0 +1,13 @@
+//
+//  D3DataManager.h
+//  D3-api-ios
+//
+//  Created by Maxim Mikheev on 17.02.13.
+//  Copyright (c) 2013 Maxim Mikheev. All rights reserved.
+//
+
+#import "D3Object.h"
+
+@interface D3DataManager : D3Object
+
+@end
---
+++ D3-api-ios/D3DataManager.m
@@ -0,0 +1,13 @@
+//
+//  D3DataManager.m
+//  D3-api-ios
+//
+//  Created by Maxim Mikheev on 17.02.13.
+//  Copyright (c) 2013 Maxim Mikheev. All rights reserved.
+//
+
+#import "D3DataManager.h"
+
+@implementation D3DataManager
+
+@end

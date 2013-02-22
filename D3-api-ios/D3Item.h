2e4e3458c32d5037c30ebc4a27e9811e4b1c037f
Maxim Mikheev <maxmikheev@gmail.com>
2013-02-22 08:44:56 +0400

    Add items partial support

--- D3-api-ios/D3Item.h
+++ D3-api-ios/D3Item.h
@@ -8,6 +8,44 @@
 
 #import "D3Object.h"
 
+typedef enum {
+    kD3ItemDisplayColorUndefined,
+    kD3ItemDisplayColorWhite,
+    kD3ItemDisplayColorBlue,
+    kD3ItemDisplayColorYellow,
+    kD3ItemDisplayColorOrange
+} D3ItemDisplayColor;
+
+typedef enum {
+    kD3ItemOwnerTypeUndefined,
+    kD3ItemOwnerTypeHero,
+    kD3ItemOwnerTypeFollowerTemplar,
+    kD3ItemOwnerTypeFollowerScoundrel,
+    kD3ItemOwnerTypeFollowerEnchantress
+} D3ItemOwnerType;
+
 @interface D3Item : D3Object
+
+@property (nonatomic, copy) NSString *itemID;
+@property (nonatomic) NSInteger itemOwnerHeroID;
+@property (nonatomic) D3ItemOwnerType itemOwnerType;
+@property (nonatomic, copy) NSString *itemName;
+@property (nonatomic, copy) NSString *itemIcon;
+@property (nonatomic) D3ItemDisplayColor itemDisplayColor;
+@property (nonatomic, copy) NSString *itemTooltipParams;
+
+#pragma mark - Init Methods
+
+/// Initialize item object with zero values
+- (id)init;
+
+/// Initialize item object with defined values
+- (id)initWithID:(NSString *)itemIDVal
+     ownerHeroID:(NSInteger)itemOwnerHeroIDVal
+       ownerType:(D3ItemOwnerType)itemOwnerTypeVal
+            name:(NSString *)itemNameVal
+            icon:(NSString *)itemIconVal
+    displayColor:(D3ItemDisplayColor)itemDisplayColorVal
+   tooltipParams:(NSString *)itemTooltipParamsVal;
 
 @end
--- D3-api-ios/D3Item.m
+++ D3-api-ios/D3Item.m
@@ -10,4 +10,39 @@
 
 @implementation D3Item
 
+@synthesize itemID, itemOwnerHeroID, itemOwnerType, itemName, itemIcon, itemDisplayColor, itemTooltipParams;
+
+#pragma mark - Init Methods
+
+/// Initialize item object with zero values
+- (id)init {
+    return [self initWithID:@""
+                ownerHeroID:0
+                  ownerType:kD3ItemOwnerTypeUndefined
+                       name:@""
+                       icon:@""
+               displayColor:kD3ItemDisplayColorUndefined
+              tooltipParams:@""];
+}
+
+/// Initialize item object with defined values
+- (id)initWithID:(NSString *)itemIDVal
+     ownerHeroID:(NSInteger)itemOwnerHeroIDVal
+       ownerType:(D3ItemOwnerType)itemOwnerTypeVal
+            name:(NSString *)itemNameVal
+            icon:(NSString *)itemIconVal
+    displayColor:(D3ItemDisplayColor)itemDisplayColorVal
+   tooltipParams:(NSString *)itemTooltipParamsVal {
+    if (self = [super init]) {
+        [self setItemID:itemIDVal];
+        [self setItemOwnerHeroID:itemOwnerHeroIDVal];
+        [self setItemOwnerType:itemOwnerTypeVal];
+        [self setItemName:itemNameVal];
+        [self setItemIcon:itemIconVal];
+        [self setItemDisplayColor:itemDisplayColorVal];
+        [self setItemTooltipParams:itemTooltipParamsVal];
+    }
+    return self;
+}
+
 @end

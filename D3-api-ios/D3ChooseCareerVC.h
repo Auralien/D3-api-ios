ccef5d9f34641dbf48476c47a9bac1b62daec115
Maxim Mikheev <maxmikheev@gmail.com>
2013-03-09 09:53:24 +0400

    Add choose career view

---
+++ D3-api-ios/D3ChooseCareerVC.h
@@ -0,0 +1,13 @@
+//
+//  D3ChooseCareerVC.h
+//  D3-api-ios
+//
+//  Created by Maxim Mikheev on 09.03.13.
+//  Copyright (c) 2013 Maxim Mikheev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface D3ChooseCareerVC : UIViewController
+
+@end
---
+++ D3-api-ios/D3ChooseCareerVC.m
@@ -0,0 +1,38 @@
+//
+//  D3ChooseCareerVC.m
+//  D3-api-ios
+//
+//  Created by Maxim Mikheev on 09.03.13.
+//  Copyright (c) 2013 Maxim Mikheev. All rights reserved.
+//
+
+#import "D3ChooseCareerVC.h"
+
+@interface D3ChooseCareerVC ()
+
+@end
+
+@implementation D3ChooseCareerVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
